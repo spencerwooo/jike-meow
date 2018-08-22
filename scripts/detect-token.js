626a4b28150f961f590a0777c5f21da2d55a6c70
JayYan <jailjt.editor@gmail.com>
2018-08-22 13:31:10 +0800

    fix: 时间戳格式

--- scripts/detect-token.js
+++ scripts/detect-token.js
@@ -1,12 +1,31 @@
 var token = localStorage["auth-token"]
 var access_token = localStorage["access-token"]
+
+Date.prototype.toIsoString = function () {
+  var tzo = -this.getTimezoneOffset(),
+    dif = tzo >= 0 ? '+' : '-',
+    pad = function (num) {
+      var norm = Math.floor(Math.abs(num));
+      return (norm < 10 ? '0' : '') + norm;
+    };
+  return this.getFullYear() +
+    '-' + pad(this.getMonth() + 1) +
+    '-' + pad(this.getDate()) +
+    'T' + pad(this.getHours()) +
+    ':' + pad(this.getMinutes()) +
+    ':' + pad(this.getSeconds()) +
+    dif + pad(tzo / 60) +
+    ':' + pad(tzo % 60);
+}
+
 chrome.storage.local.get(null, function (result) {
+  console.log(result)
   if (!token || !access_token) {
     if (result.token && result["access-token"]) {
       var date = new Date()
       localStorage.setItem("auth-token", result.token)
       localStorage.setItem("access-token", result["access-token"])
-      localStorage.setItem("token-timestamp", date.toISOString())
+      localStorage.setItem("token-timestamp", date.toIsoString())
       location.href = "https://web.okjike.com/"
       chrome.runtime.sendMessage({
         token: result.token
--- scripts/store-token.js
+++ scripts/store-token.js
@@ -1,9 +1,26 @@
 // 在网页 LocalStorage 部署 Storage 传来的 Token
+Date.prototype.toIsoString = function () {
+  var tzo = -this.getTimezoneOffset(),
+    dif = tzo >= 0 ? '+' : '-',
+    pad = function (num) {
+      var norm = Math.floor(Math.abs(num));
+      return (norm < 10 ? '0' : '') + norm;
+    };
+  return this.getFullYear() +
+    '-' + pad(this.getMonth() + 1) +
+    '-' + pad(this.getDate()) +
+    'T' + pad(this.getHours()) +
+    ':' + pad(this.getMinutes()) +
+    ':' + pad(this.getSeconds()) +
+    dif + pad(tzo / 60) +
+    ':' + pad(tzo % 60);
+}
+
 chrome.storage.local.get(null, function (result) {
   var date = new Date()
   localStorage.setItem("auth-token", result.token)
   localStorage.setItem("access-token", result["access-token"])
-  localStorage.setItem("token-timestamp", date.toISOString())
+  localStorage.setItem("token-timestamp", date.toIsoString())
   location.href = "https://web.okjike.com/"
   chrome.runtime.sendMessage({
     token: result.token
