2f136600de9050ed7fdea44749986fd5112577fa
JayYan <jailjt.editor@gmail.com>
2018-09-23 20:26:57 +0800

    fixed: 网页登录代码简化

--- scripts/store-token.js
+++ scripts/store-token.js
@@ -1,28 +1,17 @@
-var token = localStorage['auth-token']
-var access_token = localStorage['access-token']
-
-// 在网页 LocalStorage 部署 Storage 传来的 Token
-chrome.storage.local.get(null, function (result) {
-
-  // 判断当面页面是否为 "web.okjike.com"
-  // 如果是, 这部署相关 token 至 localStorage 中并刷新页面
-  if (window.location.host.indexOf('web.okjike.com') > -1) {
-    var date = new Date()
-    localStorage.setItem('auth-token', result.token)
-    localStorage.setItem('access-token', result['access-token'])
-    localStorage.setItem('token-timestamp', date.toIsoString())
-    location.reload()
-  } else {
-    alert('请在即刻网页版（https://web.okjike.com）下使用该功能')
-  }
-})
+chrome.storage.local.get(null, (res) => {
+  var date = new Date();
+  localStorage.setItem('auth-token', res.token);
+  localStorage.setItem('access-token', res['access-token']);
+  localStorage.setItem('token-timestamp', date.toIsoString());
+  location.reload();
+});
 
 // 时间戳生成公式
 Date.prototype.toIsoString = function () {
   var tzo = -this.getTimezoneOffset(),
-    dif = tzo >= 0 ? '+' : '-', 
+    dif = tzo >= 0 ? '+' : '-',
     pad = function (num) {
-      var norm = Math.floor(Math.abs(num))
+      var norm = Math.floor(Math.abs(num));
       return (norm < 10 ? '0' : '') + norm
     }
   return this.getFullYear() +
@@ -32,5 +21,5 @@ Date.prototype.toIsoString = function () {
     ':' + pad(this.getMinutes()) +
     ':' + pad(this.getSeconds()) +
     dif + pad(tzo / 60) +
-    ':' + pad(tzo % 60)
+    ':' + pad(tzo % 60);
 }
