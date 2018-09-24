a5e50ad86cdf6093995cb9439e15108617e20bba
JayYan <jailjt.editor@gmail.com>
2018-09-24 14:35:34 +0800

    delete: 彻底移除 socket-io 模块

--- background.js
+++ background.js
@@ -1,72 +1,5 @@
 'use strict'
 
-let notifyIO;
-
-// 监听 onMessage
-chrome.runtime.onMessage.addListener(messageCallback);
-async function messageCallback(result) {
-  if (result.access_token) {
-
-    // 建立 socket 连接
-    notifyIO = io('wss://msgcenter.jike.ruguoapp.com', {
-      query: {
-        'x-jike-access-token': await getToken()
-      },
-      reconnectionDelay: 3e3
-    });
-
-    // 监听 socket 的 message 事件
-    notifyIO.on('message', data => {
-
-      // 判断接收的 data type
-      // 这一接口不仅接收 NOTIFICATION type
-      // 还会接收 PERSONAL_UPDATE type
-      if (data.type === 'NOTIFICATION') {
-        chrome.browserAction.setBadgeText({
-          text: data.data.unreadCount === 0 ? '' : data.data.unreadCount.toString()
-        });
-      }
-    });
-
-    // 每 10 分钟刷新一次 access token 和 refresh token
-    // chrome.storage.local.get(null, res => {
-
-    //   // 避免 setInterval 多线程
-    //   clearInterval(localStorage['timerId']);
-
-    //   // 创建一个 Interval
-    //   let refreshToken = setInterval(() => {
-    //     axios({
-    //       url: 'https://app.jike.ruguoapp.com/app_auth_tokens.refresh',
-    //       method: 'get',
-    //       headers: {
-    //         'x-jike-refresh-token': res['refresh-token']
-    //       }
-    //     })
-    //       .then(response => {
-    //         const data = response.data;
-
-    //         // 刷新本地 token
-    //         chrome.storage.local.set({
-    //           'token': data['x-jike-refresh-token'],
-    //           'access-token': data['x-jike-access-token']
-    //         });
-
-    //         // 要求 store-token.js 在网页端覆盖新的 token
-    //         chrome.tabs.executeScript(null, { file: "scripts/store-token.js" });
-    //       })
-    //       .catch(() => {
-    //         // 即刻官网对于刷新 token 的时间控制为十四天
-    //         // 所以这里不作严格处理
-    //       });
-    //   }, 6e5);
-
-    //   // 在 localStorage 中存储 timerId 用于 clearInterval 定位
-    //   localStorage.setItem('timerId', refreshToken);
-    // });
-  }
-}
-
 // 实时更新当前用户访问的 URL
 chrome.tabs.onUpdated.addListener(function (tabid, changeinfo, tab) {
   var url = tab.url;
@@ -91,12 +24,3 @@ chrome.tabs.onUpdated.addListener(function (tabid, changeinfo, tab) {
     });
   }
 });
-
-// 获取本地 token
-const getToken = () => {
-  return new Promise(resolve => {
-    chrome.storage.local.get(null, (res) => {
-      resolve(res['access-token']);
-    });
-  });
-}
