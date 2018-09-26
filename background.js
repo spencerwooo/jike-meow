3afee267223df086099789156ee474deab1acaa5
JayYan <jailjt.editor@gmail.com>
2018-09-26 11:35:56 +0800

    delete: 去除 socket io 模块

--- background.js
+++ background.js
@@ -1,12 +1,9 @@
 'use strict'
 
-var notifyIO;
-
 // 监听 popup.js 的回调
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.logged_in === true) {
-      socketConnection();
       refreshToken();
     }
   });
@@ -36,60 +33,6 @@ chrome.tabs.onUpdated.addListener(function (tabid, changeinfo, tab) {
     });
   }
 });
-
-// socket-io 模块
-async function socketConnection() {
-  if (navigator.onLine === false) return
-
-  // 开始建立 socket 连接
-  notifyIO = io('wss://msgcenter.jike.ruguoapp.com', {
-    query: {
-      'x-jike-access-token': await getToken()
-    },
-    reconnectionDelay: 10000
-  });
-  notifyIO.on('message', data => {
-    // 判断接收的 data type
-    // 这一接口不仅接收 NOTIFICATION type
-    // 还会接收 PERSONAL_UPDATE type
-    if (data.type === 'NOTIFICATION') {
-      chrome.browserAction.setBadgeText({
-        text: data.data.unreadCount === 0 ? '' : data.data.unreadCount > 99 ? '99+' : data.data.unreadCount.toString()
-      });
-    }
-  });
-  notifyIO.on('reconnect_attempt', () => {
-    if (navigator.onLine === false) {
-      notifyIO.disconnect();
-      chrome.browserAction.setBadgeText({ text: '' });
-    }
-  });
-  notifyIO.on('reconnect_error', () => {
-    notifyIO.disconnect();
-    chrome.browserAction.setBadgeText({ text: '' });
-  });
-  notifyIO.on('disconnect', () => {
-    notifyIO.disconnect();
-    chrome.browserAction.setBadgeText({ text: '' });
-  });
-  notifyIO.on('connect_error', () => {
-    notifyIO.disconnect();
-    chrome.browserAction.setBadgeText({ text: '' });
-  });
-  notifyIO.on('error', () => {
-    notifyIO.disconnect();
-    chrome.browserAction.setBadgeText({ text: '' });
-  });
-}
-
-// 同步返回 access token
-function getToken() {
-  return new Promise(resolve => {
-    chrome.storage.local.get(null, (res) => {
-      resolve(res['access-token']);
-    });
-  });
-}
 
 // 每 10 分钟刷新一次 access token 和 refresh token
 function refreshToken() {
